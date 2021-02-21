89806c5ecba19027e1bc0b01f28a9c4106cf5a0b
idahdam <nasshitell@gmail.com>
2021-02-21 13:29:12 +0700

    but why

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -26,8 +26,9 @@ const url =  REACT_APP_CLOUD_URL;
 
 const Body = () => {
     const dayArray = ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu"]
-    const [crop, setCrop] = useState({ unit: '%', width: 30 });
+    const [crop, setCrop] = useState({ unit: '%'});
     const [completedCrop, setCompletedCrop] = useState(null);
+    const [show, setShow] = useState(false)
     const [image, setImage] = useState(null);
     const [text, setText] = useState('');
     const [button, setButton] = useState({
@@ -44,10 +45,6 @@ const Body = () => {
       width: 0,
       height: 0
     })
-    const [imageRes, setImageRes] = useState({
-      width: 0,
-      height: 0,
-    })
 
     const handleImageLoaded = (image) => {
       // console.log(image)
@@ -58,35 +55,33 @@ const Body = () => {
       // console.log({crop: crop})
     }
 
-    const handleOnCropComplete = (crop, pixelCrop) => {
+    const handleOnCropComplete = (crop) => {
       setCompletedCrop(crop)
-      console.log(crop, pixelCrop)
+      setImagePos({
+        left: crop.x,
+        top: crop.y,
+        width: crop.width,
+        height: crop.height
+      })
     }
     
     const iterOCR = (imageUrl) => {
-      var i = 0;
-      // // for(i = 0; i<7; i++){
-      //   if(button[dayArray[0]] === true){
-      // console.log(button[dayArray[0]])
+      // const rectangle = { "left": completedCrop.x, "top": completedCrop.y, "width": completedCrop.width, "height": completedCrop.height };
+      console.log(imagePos)
+      // const worker = createWorker({
+      //   logger: m => console.log(m)
+      // });
 
-      const rectangle = { left: 0, top: 0, width: 265.375, height: 694 };
-
-      const worker = createWorker({
-        logger: m => console.log(m)
-      });
-
-      // replace rectangle with imagePos
-      (async () => {
-        await worker.load();
-        await worker.loadLanguage('eng');
-        await worker.initialize('eng');
-        const { data: { text } } = await worker.recognize(imageUrl, {rectangle});
-        console.log(imagePos)
-        console.log(text);
-        // setTextArea(text);
-        await worker.terminate();
-      })();
-      imagePos["width"] += 40
+      // // replace rectangle with imagePos
+      // (async () => {
+      //   await worker.load();
+      //   await worker.loadLanguage('eng');
+      //   await worker.initialize('eng');
+      //   const { data: { text } } = await worker.recognize(imageUrl, {rectangle});
+      //   console.log(text);
+      //   // setTextArea(text);
+      //   await worker.terminate();
+      // })();
     }
 
     const setTextArea = (text) => {
@@ -94,11 +89,15 @@ const Body = () => {
     }
 
     const onChange = e => {
-        setImage(e.target.files[0]);
+      setImage(e.target.files[0]);
     };
 
     const alertPeep = () => {
       alert('on build.')
+    }
+
+    const onClickSet = (imagePos, crop) => {
+      console.log(crop)
     }
 
     const onSubmit = async () => {
@@ -118,9 +117,11 @@ const Body = () => {
             timer: 1500
           }))
           // .then(checkSize(imageUrl))
+          .then(setShow(true))
           .then(setImage(imageUrl))
-          .then(iterOCR(imageUrl))
-          .then(console.log(imageUrl))
+          .then(iterOCR(imageUrl, imagePos))
+          // .then(console.log(imageUrl))
+          // .then(console.log(imagePos))
           // .then((text) => setText(text))
           
           
@@ -129,52 +130,61 @@ const Body = () => {
             icon: 'error',
             title: err,
             text: 'Apa kamu sudah memilih file gambar? (.png/.jpg)?',
-            footer: '<a href>Why do I have this issue?</a>'
+            footer: '<a href="https://google.com">Why do I have this issue?</a>'
           })
         } 
     };
 
+
+
     return(
         <>  
-            <BodyContainer>
+          <BodyContainer>
+            {show ? 
+            <>
+              <BodyDesc>Berikut jadwal yang kamu upload: </BodyDesc>
+              <BodyImage src={image} id="canvas" alt="Jika kamu melihat ini, kamu belum mengupload atau gagal."/>
+              <BodyDesc>Silakan crop berdasarkan hari:</BodyDesc>
+              <ReactCrop 
+                src={image}
+                crop={crop}
+                onImageLoaded={handleImageLoaded}
+                onChange={(c) => handleOnCropChange(c)}
+                onComplete={(c) => handleOnCropComplete(c)}
+              />
+              <p>top: {crop.y}, left: {crop.x}, width: {crop.width}, height: {crop.height}</p>
+              <BodyButton onClick={({crop}) => onClickSet({crop})}>Set</BodyButton>
+              <BodyDesc>Berikut versi JSON:</BodyDesc>
+              <BodyTextArea placeholder="Text area ini bisa di-stretch." value={text} onChange={(text) => setTextArea(text)}></BodyTextArea>
+              <BodyDesc>Klik tombol di bawah untuk mendownload versi .ICS:</BodyDesc>
+              <BodyButton onClick={alertPeep} >Download</BodyButton>
+            </>
+            : 
+            <>
               <BodyTitle>Kamu seharian tidak ada matkul di hari apa?</BodyTitle>
               <BodyFormButton>
-                  <input type="checkbox" id="Senin" name="Senin" value="Senin" onChange={() => setButtonhandler(button, 'senin')}/>
-                  <label > Senin</label><br/>
-                  <input type="checkbox" id="Selasa" name="Selasa" value="Selasa" onChange={() => setButtonhandler(button, 'selasa')}/>
-                  <label > Selasa</label><br/>
-                  <input type="checkbox" id="Rabu" name="Rabu" value="Rabu" onChange={() => setButtonhandler(button, 'rabu')}/>
-                  <label > Rabu</label><br/>
-                  <input type="checkbox" id="Kamis" name="Kamis" value="Kamis" onChange={() => setButtonhandler(button, 'kamis')}/>
-                  <label > Kamis</label><br/>
-                  <input type="checkbox" id="Jumat" name="Jumat" value="Jumat" onChange={() => setButtonhandler(button, 'jumat')}/>
-                  <label > Jumat</label><br/>
-                  <input type="checkbox" id="Sabtu" name="Sabtu" value="Sabtu" onChange={() => setButtonhandler(button, 'sabtu')}/>
-                  <label > Sabtu</label><br/>
+                <input type="checkbox" id="Senin" name="Senin" value="Senin" onChange={() => setButtonhandler(button, 'senin')}/>
+                <label > Senin</label><br/>
+                <input type="checkbox" id="Selasa" name="Selasa" value="Selasa" onChange={() => setButtonhandler(button, 'selasa')}/>
+                <label > Selasa</label><br/>
+                <input type="checkbox" id="Rabu" name="Rabu" value="Rabu" onChange={() => setButtonhandler(button, 'rabu')}/>
+                <label > Rabu</label><br/>
+                <input type="checkbox" id="Kamis" name="Kamis" value="Kamis" onChange={() => setButtonhandler(button, 'kamis')}/>
+                <label > Kamis</label><br/>
+                <input type="checkbox" id="Jumat" name="Jumat" value="Jumat" onChange={() => setButtonhandler(button, 'jumat')}/>
+                <label > Jumat</label><br/>
+                <input type="checkbox" id="Sabtu" name="Sabtu" value="Sabtu" onChange={() => setButtonhandler(button, 'sabtu')}/>
+                <label > Sabtu</label><br/>
               </BodyFormButton>
-            <BodyTitle>Silakan upload screenshot SIAKNG-mu di bawah ini.</BodyTitle>
-            <BodyDesc>Format file berupa .jpg atau .png.</BodyDesc>
-            <BodyForm>
-                    <BodyInput type='file' name='image' onChange={onChange} />
-                    <BodyButton onClick={onSubmit} >Upload</BodyButton>
-            </BodyForm>
-                    <BodyDesc>Berikut jadwal yang kamu upload: </BodyDesc>
-                    <BodyImage src={image} id="canvas" alt="Jika kamu melihat ini, kamu belum mengupload atau gagal."/>
-                    <BodyDesc>Silakan crop berdasarkan hari:</BodyDesc>
-                    <br/>
-                    <ReactCrop 
-                      width={50}
-                      src={image}
-                      crop={crop}
-                      onImageLoaded={handleImageLoaded}
-                      onChange={(c) => handleOnCropChange(c)}
-                      onComplete={(c) => handleOnCropComplete(c)}
-                    />
-                    <BodyDesc>Berikut versi JSON:</BodyDesc>
-                    <BodyTextArea placeholder="Text area ini bisa di-stretch." value={text} onChange={() => setTextArea(text)}></BodyTextArea>
-                    <BodyDesc>Klik tombol di bawah untuk mendownload versi .ICS:</BodyDesc>
-                    <BodyButton onClick={alertPeep} >Download</BodyButton>
-            </BodyContainer>
+              <BodyTitle>Silakan upload screenshot SIAKNG-mu di bawah ini.</BodyTitle>
+              <BodyDesc>Format file berupa .jpg atau .png.</BodyDesc>
+              <BodyForm>
+                <BodyInput type='file' name='image' onChange={onChange} />
+                <BodyButton onClick={onSubmit} >Upload</BodyButton>
+              </BodyForm>
+          </>
+          }
+          </BodyContainer>
         </>
     )}   
 
